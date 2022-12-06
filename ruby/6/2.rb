8945334c69692158450548e09cc0fae460c1e340
Nick Mathis <nick@simreg.net>
2022-12-06 11:52:10 -0500

    Day 6

---
+++ ruby/6/2.rb
@@ -0,0 +1,17 @@
+#! /usr/bin/env ruby
+
+chars = File.open("full.txt").to_a.first.split('')
+
+set = []
+chars_in = 0
+chars.each_with_index do |char,i|
+	set[i % 14] = char
+	if i > 3
+		if set.uniq.size == 14
+			chars_in = i + 1
+			break;
+		end
+	end
+end
+
+puts chars_in
